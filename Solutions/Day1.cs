8d13265c6e333ec59a77726ce6ab9b0f7f248b2e
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2023-12-03 03:09:49 +0200

    Day 1 (Part One)

---
+++ Solutions/Day1.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2023.Solutions;
+
+public sealed class Day1 : BaseSolution
+{
+    public Day1() : base(1)
+    {
+    }
+
+    public override string Solve()
+    {
+        string[] input = GetInputTextLines();
+
+        return input
+              .Select(x => x.Where(Char.IsDigit))
+              .Select(x => x.Count() == 1 ? x.Concat(x) : x)
+              .Select(x => $"{x.First()}{x.Last()}")
+              .Select(Int32.Parse)
+              .Sum()
+              .ToString();
+    }
+}
